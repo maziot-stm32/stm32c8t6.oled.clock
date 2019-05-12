119f8ae6c7725913d8559e5d696926a94f26337e
mz8023yt <mz8023yt@163.com>
2019-05-12 23:32:15 +0800

    [CONF] 串口1中断不调用hal库实现

--- Src/stm32f1xx_it.c
+++ Src/stm32f1xx_it.c
@@ -205,7 +205,6 @@ void USART1_IRQHandler(void)
   /* USER CODE BEGIN USART1_IRQn 0 */
 
   /* USER CODE END USART1_IRQn 0 */
-  HAL_UART_IRQHandler(&huart1);
   /* USER CODE BEGIN USART1_IRQn 1 */
 
   /* USER CODE END USART1_IRQn 1 */
